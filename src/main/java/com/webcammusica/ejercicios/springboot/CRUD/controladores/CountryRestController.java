a4c5fa975511d729169ce9c67aa70d32f3c8f7a4
webcammusica <carlos.eduardo.diaz.penuela@gmail.com>
2021-09-14 16:24:23 -0500

    Update CountryRestController.java

--- src/main/java/com/webcammusica/ejercicios/springboot/CRUD/controladores/CountryRestController.java
+++ src/main/java/com/webcammusica/ejercicios/springboot/CRUD/controladores/CountryRestController.java
@@ -3,8 +3,7 @@ package com.webcammusica.ejercicios.springboot.CRUD.controladores;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,10 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.webcammusica.ejercicios.springboot.CRUD.dom.DOMReporte1;
 import com.webcammusica.ejercicios.springboot.CRUD.entidades.Country;
 import com.webcammusica.ejercicios.springboot.CRUD.servicios.CountryService;
-import com.webcammusica.ejercicios.springboot.CRUD.servicios.ServicioReporte1;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,9 +24,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.TypedQuery;
 import javax.validation.Valid;
 
 @RestController
